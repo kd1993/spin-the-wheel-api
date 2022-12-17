c4397391b4267c2ebd561e7a6bf64fae1a49291a
kd1993 <kevin.damas.4@gmail.com>
2022-12-17 12:07:16 -0600

    Add files via upload

---
+++ config/messages.js
@@ -0,0 +1,17 @@
+exports.messages = {
+    status: {
+        400: "Bad Request",
+        401: "Unauthorized Request",
+        404: "Not Found",
+        500: "Server Error",
+    },
+    onCreate: "Item Created Successful",
+    onUpdate: "Item Updated Successful",
+    onDelete: "Item Deleted Successful",
+    auth: {
+        userExist: "User already exist",
+        incorrectPassword: "The password is incorrect",
+        missingToken: "Missing token, permission denied",
+        invalidToken: "Invalid token"
+    }
+}
