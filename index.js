d20bf685bf16071c455ab688a3a29f3327754e70
kd1993 <kevin.damas.4@gmail.com>
2022-12-17 13:41:04 -0600

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,16 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header(
+      'Access-Control-Allow-Headers',
+      'origin, X-Requested-With,Content-Type,Accept, Authorization'
+    );
+  
+    next();
+  });
+
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
 });
